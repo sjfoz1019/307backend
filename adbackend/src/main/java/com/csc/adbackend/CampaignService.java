3db32e5351511708132adaeef5845206eaf90320
bkehle <bretkehle@comcast.net>
2020-11-30 11:50:37 -0800

    basic working database

---
+++ adbackend/src/main/java/com/csc/adbackend/CampaignService.java
@@ -0,0 +1,13 @@
+package com.csc.adbackend;
+
+import java.util.List;
+
+public interface CampaignService {
+    public List<Campaign> getAllCampaigns();
+    public Campaign getCampaign(Integer cmpId);
+    public Integer addCampaign(Campaign campaign);
+    public Integer addAd(Integer cmpId, Ad ad);
+    public void deleteAll();
+    public List<Ad> getCampaignAds(Integer cmpId);
+    public Ad getRandomAd();
+}
