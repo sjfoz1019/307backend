90426205fadb7fe98ae0b5cf0380b99a1573c656
Steven Fosmark <sjfoz1019@gmail.com>
2020-12-04 12:15:47 -0800

    DELETE campaign has correct error handling

--- adbackend/src/main/java/com/csc/adbackend/RestApi.java
+++ adbackend/src/main/java/com/csc/adbackend/RestApi.java
@@ -6,8 +6,8 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -111,10 +111,12 @@ public class RestApi {
 
         try {
             campaignService.deleteCampaign(campID);
-            responseEntity = new ResponseEntity<>("Campaign deleted.", HttpStatus.OK);
+            responseEntity = ResponseEntity.ok().build();
 
-        } catch (IllegalArgumentException e) {
-            responseEntity = new ResponseEntity<>("Campaign not found.", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (EmptyResultDataAccessException e) {
+            responseEntity = ResponseEntity.badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{\"error\":\"notFound\", \"details\":[]}");
         }
 
         return responseEntity;
